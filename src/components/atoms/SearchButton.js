57c0d969cf6938ed2e23bd595530e9c25bc3bbd1
archana1660 <archanamech16@gmail.com>
2023-09-17 23:06:31 +0530

    e-com-subscribe-page:Add subscribe page, some style to navbar.

--- src/components/atoms/SearchButton.js
+++ src/components/atoms/SearchButton.js
@@ -1,3 +1,3 @@
 export const SearchButton = () => {
-    return <button className="bg-blue-600 rounded-lg w-32 h-10 text-white">Search</button>
+    return <button className="bg-black text-white py-3 px-5 rounded-lg hover:bg-orange-500 hover:text-black">Search</button>
 }
