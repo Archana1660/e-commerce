6f9e0e8657142c8c9096c5ef7555ed808d0d64d5
archana1660 <archanamech16@gmail.com>
2023-09-22 00:15:32 +0530

    e-com-nav-panel:Add style to nav panel.

--- src/components/molecules/SearchInputAndButton.js
+++ src/components/molecules/SearchInputAndButton.js
@@ -1,9 +1,5 @@
-import { SearchButton } from "../atoms/SearchButton"
 import { SearchInput } from "../atoms/SearchInput"
 
 export const SearchInputAndButton = () => {
-    return <div>
-        <SearchInput />
-        <SearchButton />
-    </div>
+    return <SearchInput />
 }
