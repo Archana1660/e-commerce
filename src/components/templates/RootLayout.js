36c65f9e9002164a331a656482c68feaa104f1dc
archana1660 <archanamech16@gmail.com>
2023-11-05 15:38:54 +0530

    e-com-login-page:store token in the localStorage.

--- src/components/templates/RootLayout.js
+++ src/components/templates/RootLayout.js
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom"
 import { NavBarPanel } from "../organisms/NavBarPanel"
 
-
 export const RootLayout = () => {
-    return <main>
-        <header>
-            <NavBarPanel />
-        </header>
-        <Outlet />
-    </main>
+    let isToken = (window.localStorage.getItem('token') === 'undefined') ? false : true
+    const auth = { token: isToken }
+    return auth.token ?
+        <main>
+            <header>
+                <NavBarPanel />
+            </header>
+            <Outlet />
+        </main> : <Navigate to="/login" />
 }
